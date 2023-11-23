99c5826c2c7868319c847037d81cc376591cbd2e
RS120 <145163193+RS120@users.noreply.github.com>
2023-11-23 10:23:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+// Button Event Listener
+
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  let a1 = document.getElementById("answer1").value.toLowerCase();
+  let a2 = document.getElementById("answer2").value.toLowerCase();
+  let a3 = document.getElementById("answer3").value.toLowerCase();
+  let a4 = document.getElementById("answer4").value.toLowerCase();
+  let totalcorrect = 0;
+  let totalquestions = 4;
+
+  if (a1 === "false") {
+    document.getElementById("result").innerHTML = "Correct";
+    totalcorrect++;
+  } else {
+    document.getElementById("result").innerHTML = "False";
+  }
+
+  if (a2 === "22") {
+    document.getElementById("result1").innerHTML = "Correct";
+    totalcorrect++;
+  } else {
+    document.getElementById("result1").innerHTML = "False";
+  }
+
+  if (a3 === "yes") {
+    document.getElementById("result2").innerHTML = "Correct";
+    totalcorrect++;
+  } else {
+    document.getElementById("result2").innerHTML = "False";
+  }
+
+  if (a4 === "red card" || a4 === "red") {
+    document.getElementById("result3").innerHTML = "Correct";
+    totalcorrect++;
+  } else {
+    document.getElementById("result3").innerHTML = "False";
+  }
+
+  // Output
+
+  document.getElementById("output").innerHTML = totalcorrect;
+
+  let percent1 = (totalcorrect / totalquestions) * 100;
+  document.getElementById("output2").innerHTML = percent1;
+
+  if (totalcorrect === 4) {
+    document.getElementById("note").innerHTML = "Wonderful";
+  }
+  if (totalcorrect === 3) {
+    document.getElementById("note").innerHTML = "Amazing";
+  }
+  if (totalcorrect === 2) {
+    document.getElementById("note").innerHTML = "Good work";
+  }
+  if (totalcorrect <= 1) {
+    document.getElementById("note").innerHTML = "Maybe next time...";
+  }
+}
